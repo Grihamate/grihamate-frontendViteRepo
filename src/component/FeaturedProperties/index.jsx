8e0d79b94f89a228eca1ca4ac2061aa5c40aed21
Ankush Shekhawat <ankush75056@gmail.com>
2025-08-27 14:49:46 +0530

    home and rent page fix

--- src/component/FeaturedProperties/index.jsx
+++ src/component/FeaturedProperties/index.jsx
@@ -5,18 +5,18 @@ import "./style.css";
 const FeaturedProperties = () => {
     return (
         <div className="parent-div">
-             <div className="property-section">
-            <div className="section-header">
-                <div className="detail-box">
-                    <h5>Featured Properties</h5>
-                    <p>Discover the best properties handpicked for you</p>
-                </div>
+            <div className="property-section">
+                <div className="section-header">
+                    <div className="detail-box">
+                        <h5>Featured Properties</h5>
+                        <p>Discover the best properties handpicked for you</p>
+                    </div>
 
-                <button className="feature-btn">
-                   <img src={plusIcon} alt="plus" /> 
-                   <p>Post your Property</p>
-                 </button>
-            </div>
+                    <button className="feature-btn">
+                    <img src={plusIcon} alt="plus" /> 
+                    <p>Post your Property</p>
+                    </button>
+                </div>
 
             <div className="cards-container">
                 <PropertyCard/>
@@ -32,7 +32,7 @@ const FeaturedProperties = () => {
             </div>
 
             
-        </div>
+            </div>
 
         </div>
     );
