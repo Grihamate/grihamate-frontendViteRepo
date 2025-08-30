7e4760030de751dafcf71c12e2e99e70c66ae81c
Pranjal Takkar <grihamateoffical@gmail.com>
2025-08-30 12:13:45 +0530

    fix the api url

--- src/service/Registeruser.js
+++ src/service/Registeruser.js
@@ -3,7 +3,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const registerUser = async (userData) => {
   try {
-    const response = await fetch(`${BASE_URL}/register`, {
+    const response = await fetch(`${BASE_URL}api/user/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
