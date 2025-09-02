7bea9dce25fcb50b7cc1d453b63e4ac4f0c9323f
Pranjal Takkar <grihamateoffical@gmail.com>
2025-09-02 18:43:56 +0530

    fix the changes of api

--- src/service/deleteUser.js
+++ src/service/deleteUser.js
@@ -1,31 +1,4 @@
-// import { getToken } from "../utils/authUtils";
 
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-
-// export const deleteUserAccount = async (userId) => {
-//   const token = getToken();
-
-//   try {
-//     const response = await fetch(`${BASE_URL}/api/user/delete/${userId}`, {
-//       method: "DELETE",
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-
-//     if (!response.ok) {
-//       const errorData = await response.json();
-//       throw new Error(errorData.message || "Failed to delete account");
-//     }
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error deleting account:", error);
-//     throw error;
-//   }
-// };
 import { getToken } from "../utils/authUtils";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
