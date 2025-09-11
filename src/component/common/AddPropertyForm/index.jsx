04c8b19e69c68d6a47b8024532405cbce9aefafc
Ankush Shekhawat <ankush75056@gmail.com>
2025-09-11 19:08:41 +0530

    form changes added

--- src/component/common/AddPropertyForm/index.jsx
+++ src/component/common/AddPropertyForm/index.jsx
@@ -26,7 +26,22 @@ const AddPropertyModal = ({ setIsModalOpen }) => {
     monthlyRent: "",
     securityDeposit: "",
     maintenanceCharge: "",
+    amenities: [] ,
+    propertyFacing:"",
+    propertyAge:"",
+    floor:"",
+    educationName:"",
+    educationDistance:"",
+    healthName:"",
+    healthDistance:"",
+    foodName:"",
+    foodDistance:"",
+    travelName:"",
+    fullAddress:"",
+    
   });
+
+  console.log("formData--->", formData);
 
   useEffect(() => {
     document.body.classList.add("modal-open");
@@ -97,13 +112,28 @@ const handleSubmit = async (e) => {
       maintenanceCharges: formData.maintenanceCharge,
       city: formData.city,
       locality: formData.locality,
-      fullAddress: formData.fullAddress || "",
+      fullAddress: formData.fullAddress,
       description: formData.description,
       owner: formData.contactName,
       phone: formData.phoneNumber,
       email: formData.email,
       images: formData.images,
+      amenities:formData.amenities || [],
+      educationName:formData.educationName,
+      educationDistance:formData.educationDistance,
+      healthName:formData.healthName,
+      healthDistance:formData.healthDistance,
+      foodName:formData.foodName,
+      foodDistance:formData.foodDistance,
+      travelName:formData.travelName,
+      travelDistance:formData.travelDistance,
+      propertyFacing:formData.propertyFacing,
+      propertyAge:formData.propertyAge,
+      floor:formData.floor,
+       
     };
+
+      console.log("propertData--->", propertyData);
 
     const response = await addProperty(propertyData, token);
 
@@ -199,7 +229,7 @@ const handleSubmit = async (e) => {
                         <label htmlFor="furnishingStatus">Furnishing Status</label>
                         <select
                           id="furnishingStatus"
-                          className="custom-input"
+                          className="custom-select"
                           value={formData.furnishingStatus}
                           onChange={(e) =>
                             setFormData({ ...formData, furnishingStatus: e.target.value })
@@ -215,6 +245,61 @@ const handleSubmit = async (e) => {
 
                     {/* Additional Fields */}
                     <div className="basic-filter-other">
+                       {/* Area */}
+                      <div className="filter-group-other">
+                        <label htmlFor="area">Amenities & Features</label>
+                        <input
+                          id="area"
+                          className="custom-input"
+                          placeholder="Enter features"
+                          value={formData.amenities.join(", ")}
+                          onChange={(e) =>
+                            setFormData({
+                              ...formData,
+                              amenities: e.target.value.split(",").map((item) => item.trim()),
+                            })
+                          }
+                         
+                        />
+                      </div>
+
+                       <div className="filter-group-other">
+                        <label htmlFor="area">Property Facing</label>
+                        <select
+                          id="area"
+                          className="custom-select"
+                           value={formData.pr}
+                          onChange={(e) =>
+                            setFormData({ ...formData, propertyFacing: e.target.value })
+                          } 
+                        >
+                          <option value="">Select Property Facing</option>
+                          <option value="east">East</option>
+                          <option value="west">West</option>
+                          <option value="north">North</option>
+                          <option value="south">South</option>
+                          
+                        </select>
+                      </div>
+
+                       <div className="filter-group-other">
+                        <label htmlFor="area">Property Age</label>
+                        <select
+                          id="area"
+                          className="custom-select"
+                          value={formData.propertyAge}
+                          onChange={(e) =>
+                            setFormData({ ...formData, propertyAge: e.target.value })
+                          }                    
+                        >
+                          <option value="">Select Property Age</option>
+                          <option value="1-5 years">1-5 years</option>
+                          <option value="5-10 years">5-10 years</option>
+                          <option value="10-15 years">10-15 years</option>
+                        
+                        </select>
+                      </div>
+
                       {/* BHK/Type */}
                       <div className="filter-group-other">
                         <label htmlFor="bhkType">BHK/Type</label>
@@ -234,19 +319,22 @@ const handleSubmit = async (e) => {
                         </select>
                       </div>
 
-                      {/* Area */}
                       <div className="filter-group-other">
-                        <label htmlFor="area">Area (sq ft) *</label>
-                        <input
-                          id="area"
-                          className="custom-input"
-                          placeholder="Enter area in sq ft"
-                          value={formData.area}
+                        <label htmlFor="floor">Floor</label>
+                        <select
+                          id="floor"
+                          className="custom-select"
+                          value={formData.floor}
                           onChange={(e) =>
-                            setFormData({ ...formData, area: e.target.value })
+                            setFormData({ ...formData, floor: e.target.value })
                           }
-                        />
+                        >
+                          <option value="">Select Floor</option>
+                          <option value="1 floor">1st Floor</option>
+                          <option value="2 flooe">2nd Floor</option>
+                        </select>
                       </div>
+                  
 
                       {/* Bathrooms */}
                       <div className="filter-group-other">
@@ -292,6 +380,20 @@ const handleSubmit = async (e) => {
                           value={formData.securityDeposit}
                           onChange={(e) =>
                             setFormData({ ...formData, securityDeposit: e.target.value })
+                          }
+                        />
+                      </div>
+
+                        {/* Area */}
+                      <div className="filter-group-other">
+                        <label htmlFor="area">Area (sq ft) *</label>
+                        <input
+                          id="area"
+                          className="custom-input"
+                          placeholder="Enter area in sq ft"
+                          value={formData.area}
+                          onChange={(e) =>
+                            setFormData({ ...formData, area: e.target.value })
                           }
                         />
                       </div>
@@ -344,12 +446,26 @@ const handleSubmit = async (e) => {
                       </div>
                     </div>
 
+                         {/* full address */}
+                    <div className="textarea-filter">
+                      <label htmlFor="fullAddress">Full address</label>
+                      <textarea
+                        id="fullAddress"
+                        placeholder="Enter Complete address"
+                        value={formData.fullAddress}
+                        onChange={(e) =>
+                          setFormData({ ...formData, fullAddress: e.target.value })
+                        }
+                      
+                      />
+                    </div>
+
                     {/* Description */}
                     <div className="textarea-filter">
                       <label htmlFor="description">Description</label>
                       <textarea
                         id="description"
-                        placeholder="Enter property description"
+                        placeholder="Description your property, amenities, neaby facilities,etc..."
                         value={formData.description}
                         onChange={(e) =>
                           setFormData({ ...formData, description: e.target.value })
@@ -357,11 +473,155 @@ const handleSubmit = async (e) => {
                       />
                     </div>
 
+                  
+
                     {/* Images Upload */}
                     <div className="textarea-filter">
                       <label htmlFor="Property Images">Property Images</label>
                       <ImageUpload />
                     </div>
+                  </div>
+
+                  
+                  {/* Basic Details */}
+                  <div className="form-group">
+                    <label className="form-group-label">What's nearby</label>
+
+
+                    <div className="basic-filter">
+                      {/* Property Title */}
+                      <div className="filter-group">
+                        <label htmlFor="educationName">Education</label>
+                        <input
+                          id="educationName"
+                          className="custom-input"
+                          placeholder="Enter near Education details"
+                          value={formData.educationName}
+                          onChange={(e) =>
+                            setFormData({ ...formData, educationName: e.target.value })
+                          }
+                       
+                        />
+                      </div>
+
+                      {/* Distance */}
+                      <div className="filter-group">
+                        <label htmlFor="educationDistance">Distance</label>
+                        <input
+                          id="educationDistance"
+                          className="custom-input"
+                          placeholder="Enter distance"
+                          value={formData.educationDistance}
+                          onChange={(e) =>
+                            setFormData({ ...formData, educationDistance: e.target.value })
+                          }
+                        
+                        />
+                  
+                      </div>
+                    </div>
+
+                    {/* Health */}
+
+                    <div className="basic-filter">
+                         <div className="filter-group">
+                        <label htmlFor="healthName">Health & Medicine</label>
+                        <input
+                          id="healthName"
+                          className="custom-input"
+                          placeholder="Enter Health and medicine details"
+                          value={formData.healthName}
+                          onChange={(e) =>
+                            setFormData({ ...formData, healthName: e.target.value })
+                          }
+                         
+                        />
+                      </div>
+                       {/* Distance */}
+                      <div className="filter-group">
+                        <label htmlFor="healthDistance">Distance</label>
+                        <input
+                          id="healthDistance"
+                          className="custom-input"
+                          placeholder="Enter distance"
+                          value={formData.healthDistance}
+                          onChange={(e) =>
+                            setFormData({ ...formData, healthDistance: e.target.value })
+                          }
+                         
+                        />
+                  
+                      </div>
+
+                    </div>
+                      
+                    <div className="basic-filter">
+                      
+                       <div className="filter-group">
+                        <label htmlFor="foodName">Food</label>
+                        <input
+                          id="foodName"
+                          className="custom-input"
+                          placeholder="Enter Food details"
+                          value={formData.foodName}
+                          onChange={(e) =>
+                            setFormData({ ...formData, foodName: e.target.value })
+                          }
+                        
+                        />
+                      </div>
+
+                       {/* Distance */}
+                      <div className="filter-group">
+                        <label htmlFor="foodDistance">Distance</label>
+                        <input
+                          id="foodDistance"
+                          className="custom-input"
+                          placeholder="Enter distance"
+                          value={formData.foodDistance}
+                          onChange={(e) =>
+                            setFormData({ ...formData, foodDistance: e.target.value })
+                          }
+                       
+                        />
+                  
+                      </div>
+                    </div>
+
+                    <div className="basic-filter">
+                         <div className="filter-group">
+                        <label htmlFor="travelName">Travel</label>
+                        <input
+                          id="travelName"
+                          className="custom-input"
+                          placeholder="Enter Travel details"
+                          value={formData.travelName}
+                          onChange={(e) =>
+                            setFormData({ ...formData, travelName: e.target.value })
+                          }
+                          
+                        />
+                      
+                      </div>
+
+                       {/* Distance */}
+                      <div className="filter-group">
+                        <label htmlFor="travelDistance">Distance</label>
+                        <input
+                          id="travelDistance"
+                          className="custom-input"
+                          placeholder="Enter distance"
+                          value={formData.travelDistance}
+                          onChange={(e) =>
+                            setFormData({ ...formData, travelDistance: e.target.value })
+                          }
+                      
+                        />
+                  
+                      </div>
+                    </div>
+                    
+                    
                   </div>
 
                   {/* Contact Information */}
@@ -395,7 +655,7 @@ const handleSubmit = async (e) => {
                       </div>
                     </div>
 
-                    <div className="basic-filter">
+                    <div className="textarea-filter">
                       <label htmlFor="email">Email Address</label>
                       <input
                         id="email"
